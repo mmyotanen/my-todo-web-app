f32abbc74cd59eadf265c8ebf2537a6eac1173d7
Miika Myötänen <miikamyotanen@gmail.com>
2023-05-08 16:56:16 +0300

    added to functions.py

--- functions.py
+++ functions.py
@@ -1,3 +1,6 @@
+from PIL import Image
+
+
 FILEPATH = "todos.txt"
 
 
@@ -13,4 +16,10 @@ def get_todos(filepath=FILEPATH):
 def add_todos(todos_arg, filepath=FILEPATH):
     with open(filepath, 'w') as file_local:
         file_local.writelines(todos_arg)
+
+
+def convert_image_to_grayscale(image):
+    img = Image.open(image)
+    gray_img = img.convert("L")
+    return gray_img
 
--- pages/grayscale.py
+++ pages/grayscale.py
@@ -1,7 +1,7 @@
+import sys
+sys.path.insert(1, '\\web_app1')
 import streamlit as st
-from PIL import Image
-
-
+import functions
 st.title("Color to Grayscale Converter")
 uploaded_image = st.file_uploader("Upload Image")
 
@@ -9,14 +9,10 @@ with st.expander("Start Camera"):
     camera_image = st.camera_input("Camera")
 
 if camera_image:
-    img = Image.open(camera_image)
-    gray_img = img.convert("L")
-
+    gray_img = functions.convert_image_to_grayscale(camera_image)
     st.image(gray_img)
 if uploaded_image:
-    img_from_file = Image.open(uploaded_image)
-    gray_img_file = img_from_file.convert("L")
-
+    gray_img_file = functions.convert_image_to_grayscale(uploaded_image)
     st.image(gray_img_file)
 
 
